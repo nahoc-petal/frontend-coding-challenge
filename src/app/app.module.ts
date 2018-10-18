0c5bb1b4d2f0686161055e1503127138e7720eea
nahoc-petal <ccarpentier@petalmd.com>
2018-10-18 13:57:37 -0400

    added simple header and theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ResultsComponent } from './results/results.component';
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +27,12 @@ import { ResultsComponent } from './results/results.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
